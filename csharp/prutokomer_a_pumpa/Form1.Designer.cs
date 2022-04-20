30866a14ce6c31bca457e738ef0af166ac2e1363
tomasvicar <tomasvicar@gmail.com>
2022-04-20 17:30:46 +0200

    flow reader in progress

---
+++ csharp/prutokomer_a_pumpa/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace prutokomer_a_pumpa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label_COM_flow = new System.Windows.Forms.Label();
+            this.chart_flow = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_flow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_COM_flow
+            // 
+            this.label_COM_flow.AutoSize = true;
+            this.label_COM_flow.ForeColor = System.Drawing.Color.Red;
+            this.label_COM_flow.Location = new System.Drawing.Point(286, 9);
+            this.label_COM_flow.Name = "label_COM_flow";
+            this.label_COM_flow.Size = new System.Drawing.Size(20, 17);
+            this.label_COM_flow.TabIndex = 0;
+            this.label_COM_flow.Text = "NA";
+            this.label_COM_flow.UseCompatibleTextRendering = true;
+            this.label_COM_flow.Click += new System.EventHandler(this.COM_flow_Click);
+            // 
+            // chart_flow
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_flow.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_flow.Legends.Add(legend1);
+            this.chart_flow.Location = new System.Drawing.Point(12, 29);
+            this.chart_flow.Name = "chart_flow";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart_flow.Series.Add(series1);
+            this.chart_flow.Size = new System.Drawing.Size(725, 306);
+            this.chart_flow.TabIndex = 1;
+            this.chart_flow.Text = "flow";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 631);
+            this.Controls.Add(this.chart_flow);
+            this.Controls.Add(this.label_COM_flow);
+            this.Name = "Form1";
+            this.Text = "Flowmeter and pump";
+            ((System.ComponentModel.ISupportInitialize)(this.chart_flow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_COM_flow;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_flow;
+    }
+}
+
---
+++ csharp/prutokomer_a_pumpa/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace prutokomer_a_pumpa
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFlowSerial();
+        }
+
+        private void COM_flow_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void openFlowSerial()
+        {
+            label_COM_flow.Text = "Connecting";
+            label_COM_flow.ForeColor = System.Drawing.Color.Gray;
+
+            string[] arrayComPortsNames = SerialPort.GetPortNames();
+
+            if (!(serialPort == null))
+{
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    Thread.Sleep(3000);
+                }
+            }
+            foreach (string port in arrayComPortsNames)
+            {
+                Console.WriteLine(port);
+                try
+                {
+                    serialPort = new SerialPort(port, 115200);
+
+                    serialPort.Open();
+                    serialPort.WriteTimeout = 1000;
+                    serialPort.ReadTimeout = 1000;
+
+                    Thread.Sleep(100);
+                    string message1 = serialPort.ReadLine();
+
+
+                    Console.WriteLine(message1);
+
+                    if ((message1.Trim().Length == 7) & message1.StartsWith("F"))
+                    {
+                        label_COM_flow.Text = port;
+                        label_COM_flow.ForeColor = System.Drawing.Color.Green;
+                        serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                        return;
+                    }
+                    else
+                    {
+                        serialPort.Close();
+
+                    }
+
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("time out");
+                    serialPort.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    serialPort.Close();
+                }
+            }
+            label_COM_flow.Text = "NA";
+            label_COM_flow.ForeColor = System.Drawing.Color.Red;
+
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting().Substring(1).Trim();
+
+            Console.WriteLine(indata);
+
+
+        }
+
+
+
+    }
+
+
+
+}
